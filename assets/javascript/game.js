affc83d0d7819a84ae2763e817154e9a844db6d7
EMalley <EMalley998@gmail.com>
2018-11-19 12:34:49 -0500

    finalBeforeClass

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,12 +5,11 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var lettersGuessed = [];     //set pushes to this. 
-
 // reference To DOM Elements
 var $wins = document.getElementById("wins")
 var $losses = document.getElementById("losses")
-var $guessesLeft = document.getElementById("guesses-Left")
-var $incorrectGuesses = document.getElementById("incorrect-Guesses")
+var $guessesLeft = document.getElementById("guessesLeft")
+var $incorrectLetters = document.getElementById("incorrectLetters")
 
 // randomly Chooses a letter from the Array
 chosenLetter = letters[Math.floor(Math.random() * letters.length)];
@@ -19,20 +18,31 @@ console.log(chosenLetter)
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
+  
 
     if (userGuess === chosenLetter) {
         wins++;
+        guessesLeft = 10
+        lettersGuessed = []
+       
     };
+
     if (userGuess != chosenLetter) {
-        guessesLeft--
+        guessesLeft --;
+        lettersGuessed.push(userGuess);
     };
+
     if (guessesLeft === 0) {
         losses++
+        guessesLeft = 10
+        lettersGuessed = []
     }
 
 
 // logs information from game to DOM
 $wins.textContent = wins;
 $losses.textContent = losses;
-$incorrectGuesses.textContent = lettersGuessed
+$incorrectLetters.textContent = lettersGuessed;
+$guessesLeft.textContent = guessesLeft;
 }
+
